b04867cca892db234c22c585d4ed86d3939d5dd5
Ricardo Andrés Bello <ricardoaandres@me.com>
2014-08-21 15:23:08 -0400

    Add home_page_features tab to spree_admin

--- app/overrides/admin/add_home_page_features_tab.rb
+++ app/overrides/admin/add_home_page_features_tab.rb
@@ -1,6 +1,5 @@
 Deface::Override.new(
-  virtual_path: "spree/layouts/admin", 
+  virtual_path: "spree/admin/shared/_menu",
   partial: 'spree/admin/home_page_features/tab',
   insert_bottom: "[data-hook='admin_tabs']",
   name: 'add_home_page_features_tab')
-
